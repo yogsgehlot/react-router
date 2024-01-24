06f76228d9b165f60ca0da1a3428d473c6dee115
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2024-01-24 16:36:44 +0530

    update

---
+++ src/components/Github/Github.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+export default function Github() {
+
+  /*
+  const [data, setData] = useState([])
+
+  useEffect(()=>{
+    fetch('https://api.github.com/users/yogsgehlot')
+    .then(response => response.json())
+    .then(data =>{
+      setData(data)
+      console.log(data)
+    })
+  },[])
+  */
+
+  const data = useLoaderData()
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+        Github followers: {data.followers}
+        <img src={data.avatar_url} alt="Git picture" width={300} />
+      
+    </div>
+  )
+}
+
+export const githubInfoLoader = async ()=>{
+  const response = await fetch('https://api.github.com/users/yogsgehlot')
+  return response.json()
+}
